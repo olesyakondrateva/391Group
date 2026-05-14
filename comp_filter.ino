7a7c4cbc4418c2f19921780b0fe835b5fee9f905
Sidhant Sidana <148929557+drsid04@users.noreply.github.com>
2026-05-14 13:09:33 -0700

    Update comp_filter.ino

--- comp_filter.ino
+++ comp_filter.ino
@@ -20,27 +20,34 @@ void loop() {
   float ax, ay, az;
   float gx, gy, gz;
   float accAngle;
+  const float k =0.9;
+  float filterAngle;
+
   //Time difference
   unsigned long currentTime = micros();
   float dt = (currentTime - prevTime) / 1000000.0;
   prevTime = currentTime;
 
-  if (IMU.gyroscopeAvailable()) {
+  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()){
+    // Gyro Angle
     IMU.readGyroscope(gx, gy, gz);
     // θg = θ0 + gx * Δt
-    gyroAngle = (gyroAngle + (gx*(-1)) * dt);
+    gyroAngle = (gyroAngle + (gx*(-1)) * dt); // multiplied gx by -1 to align polarity with sensor orientation
     Serial.print(gyroAngle);
     Serial.print(" ");
-    // Serial.println(" deg");
-    
-  }
-    if (IMU.accelerationAvailable()) {
-    IMU.readAcceleration(ax, ay, az);
+    Serial.println(" deg");
 
+    // Accelerometer Angle 
+    IMU.readAcceleration(ax, ay, az);
     accAngle = atan2 (ay, az) * 180/PI; 
     Serial.println(accAngle);
     Serial.print(" ");
-    
+
+    // Complimentary Filter
+    filterAngle = k*gyroAngle + (1-k)*accAngle;
+    Serial.println(filterAngle);
+    Serial.print(" deg");
   }
+
  delay(100); 
 }
