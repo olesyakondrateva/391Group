3b955b5cbaab67f7aeb397e4bd24178623894d41
sam-min <86676133+sam-min@users.noreply.github.com>
2026-05-14 12:52:46 -0700

    Update tiltAngle_Gyro.ino

--- tiltAngle_Gyro.ino
+++ tiltAngle_Gyro.ino
@@ -2,22 +2,22 @@
   Arduino BMI270 - Simple Gyroscope
 
   This example reads the gyroscope values from the BMI270
-  sensor and continuously prints them to the Serial Monitor
+  sensor, calculates the gyro angle, the time stamp, 
+  and continuously prints them to the Serial Monitor
   or Serial Plotter.
 
   The circuit:
   - Arduino Nano 33 BLE Sense Rev2
 
-  created 10 Jul 2019
-  by Riccardo Rizzo
+  Reference code by Riccardo Rizzo
 
-  This example code is in the public domain.
-*/
+  */
 
 #include "Arduino_BMI270_BMM150.h"
 
 float gyro_angle = 0.0;
 unsigned long prevTime = 0;
+float startTime = 0.0;
 
 void setup() {
   Serial.begin(9600);
@@ -28,13 +28,13 @@ void setup() {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
+  startTime = micros();
   Serial.print("Gyroscope sample rate = ");
   Serial.print(IMU.gyroscopeSampleRate());
   Serial.println(" Hz");
   Serial.println();
   Serial.println("Gyroscope in degrees/second");
   Serial.println("X\tY\tZ");
-
   prevTime = micros();
   
 }
@@ -58,7 +58,10 @@ void loop() {
 
     // θg = θ0 + gx * Δt
     gyro_angle = gyro_angle + x * dt;
-    Serial.print (x);
+    Serial.print("Time: ");
+    Serial.print((micros() - startTime)/1000000.0);
+    Serial.print(" s \t");
+    Serial.print(x);
     Serial.print(" deg/s \t Angle: ");
     Serial.print(gyro_angle);
     Serial.println(" deg");
