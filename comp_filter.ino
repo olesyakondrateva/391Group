2bbf3ad7cacf4ff39843e0bfae3d25cc12ed291b
Sidhant Sidana <148929557+drsid04@users.noreply.github.com>
2026-05-14 11:54:10 -0700

    Update comp_filter.ino

--- comp_filter.ino
+++ comp_filter.ino
@@ -1,31 +1,15 @@
-
 #include "Arduino_BMI270_BMM150.h"
-float gyro_angle = 0.0;
+float gyroAngle = 0.0;
 unsigned long prevTime = 0;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  //Serial.println("Started");
 
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
-  //Serial.print("Gyroscope sample rate = ");
-  //Serial.print(IMU.gyroscopeSampleRate());
-  //Serial.println(" Hz");
-  //Serial.println();
-  //Serial.println("Gyroscope in degrees/second");
-  //Serial.println("X\tY\tZ");
-
-
-  //Serial.print("Accelerometer sample rate = ");
-  //Serial.print(IMU.accelerationSampleRate());
-  //Serial.println(" Hz");
-  //Serial.println();
-  //Serial.println("Acceleration in G's");
-  //Serial.println("X\tY\tZ");
 
   prevTime = micros();
   }
@@ -33,51 +17,32 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  float x, y, z;
-  float tiltAngle;
-  float tiltAngledeg;
-
+  float ax, ay, az;
+  float gx, gy, gz;
+  float accAngle;
+  //Time difference
+  unsigned long currentTime = micros();
+  float dt = (currentTime - prevTime) / 1000000.0;
+  prevTime = currentTime;
 
   if (IMU.gyroscopeAvailable()) {
-    IMU.readGyroscope(x, y, z);
-
-    //Serial.print(x);
-    //Serial.print('\t');
-    //Serial.print(y);
-    //Serial.print('\t');
-    //Serial.println(z);
-
-    //Time difference
-    unsigned long currentTime = micros();
-    float dt = (currentTime - prevTime) / 1000000.0;
-    prevTime = currentTime;
-  
-
+    IMU.readGyroscope(gx, gy, gz);
     // θg = θ0 + gx * Δt
-    gyro_angle = (gyro_angle + (x*(-1)) * dt);
+    gyroAngle = (gyroAngle + (gx*(-1)) * dt);
     //Serial.print (x);
     //Serial.print(" deg/s \t Angle: ");
-    Serial.print(gyro_angle);
+    Serial.print(gyroAngle);
     Serial.print(" ");
     // Serial.println(" deg");
     
   }
     if (IMU.accelerationAvailable()) {
-    IMU.readAcceleration(x, y, z);
+    IMU.readAcceleration(ax, ay, az);
 
-    //Serial.print(x);
-    //Serial.print('\t');
-    //Serial.print(y);
-    //Serial.print('\t');
-    //Serial.println(z);
-
-    tiltAngle = atan2 (y, z);
-    tiltAngledeg = tiltAngle * 180/PI; 
-    //Serial.println(tiltAngle);
-    Serial.println(tiltAngledeg);
+    accAngle = atan2 (ay, az) * 180/PI; 
+    Serial.println(accAngle);
     Serial.print(" ");
     
-
   }
  delay(100); 
 }
